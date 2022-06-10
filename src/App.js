1bf3b4f25803c12d5053858f525784bf8fdfd779
Adrianibarra97 <adrian__ibarra@hotmail.com>
2022-06-10 05:26:47 -0300

    Creacion de las rutas del portfolio.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './css/App.css';
 export const App = () => {
     return (
 		<div className="App">
-			
+			<h1>App activa</h1>
 		</div>
 	);
 }
---
+++ src/routers/PrincipalRouters.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navigation } from '../components/Navigation';
+import { Home } from '../components/Home';
+import { Portfolio } from '../components/Portfolio';
+import { Services } from '../components/Services';
+import { Curriculum } from '../components/Curriculum';
+import { Contact } from '../components/Contact';
+import { Footer } from '../components/Footer';
+
+export const PrincipalRouters = () => {
+    return (
+        <BrowserRouter>
+            {/* Header and Nav */}
+            <Navigation/>
+            
+            {/* Main content */}
+            <Routes>
+                <Route path="/" element={ <Home/> }/>
+                <Route path="/home" element={ <Home/> }/>
+                <Route path="/portfolio" element={ <Portfolio/> }/>
+                <Route path="/services" element={ <Services/> }/>
+                <Route path="/curriculum" element={ <Curriculum/> }/>
+                <Route path="/contact" element={ <Contact/> }/>
+            </Routes>
+
+            {/* Footer */}
+            <Footer/>
+        </BrowserRouter>
+    );
+}
